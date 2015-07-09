1fd3d62905beb98648f56c1ee9ca534194818ba2
Beilin Li <bli0042@Beilins-MBP.wv.cc.cmu.edu>
2015-07-09 12:04:52 -0400

    server coupled to Cellblender now

--- __init__.py
+++ __init__.py
@@ -1,4 +1,14 @@
 import os
+import sys
+import subprocess
+import shutil
+
+def find_in_path(program_name):
+	for path in os.environ.get('PATH', '').split(os.pathsep):
+		full_name = os.path.join(path, program_name)
+		if os.path.exists(full_name) and not os.path.isdir(full_name):
+			return full_name
+	return None
 
 def get_name():
 	return ("Browser Plotter")
@@ -7,5 +17,18 @@ def requirements_met():
 	return True
 
 def plot(data_path, plot_spec):
-	print(data_path)
-	print(plot_spec)
+	program_path = os.path.dirname(__file__)
+	python_cmd = find_in_path("python3")
+	print(program_path)
+	#python_cmd = shutil.which("python", mode=os.X_OK)
+
+	if python_cmd is None:
+		print("Unable to plot: python not found in path")
+	else:
+		plot_cmd = []
+		plot_cmd.append(python_cmd)
+		plot_cmd.append(os.path.join(program_path, "server.py"))
+
+		print ("Plotting from: " + data_path)
+		print ("Plotting with: \"" + ' '.join(plot_cmd) + "\"")
+		pid = subprocess.Popen(plot_cmd, cwd = program_path)
